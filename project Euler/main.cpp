53c017283bea536c261380e426b18c8676d0b734
stratis-vip <stratis.vip@gmail.com>
2020-03-22 21:49:06 +0200

    Initial Commit

---
+++ project Euler/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  project Euler
+//
+//  Created by Στρατής Χριστοδούλου on 22/3/20.
+//  Copyright © 2020 Στρατής Χριστοδούλου. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
